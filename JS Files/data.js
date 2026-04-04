6e384ade23c90aebf448ca1b4f486526f5ac0c06
Aoling Wang <aoling.wang@gmail.com>
2026-04-04 18:24:42 -0400

    Base Outline Commit
    An updated front end skeleton concept

---
+++ JS Files/data.js
@@ -0,0 +1,30 @@
+export const menuArray = [
+    {
+        name: "Dragon Roll",
+        ingredients: ["eel", "cucumber", "avocado", "shrimp tempura"],
+        price: 14,
+        emoji: "🐉",
+        id: 0,
+    },
+    {
+        name: "California Roll",
+        ingredients: ["avocado", "cucumber", "crab meat"],
+        price: 12,
+        emoji: "🦀",
+        id: 1
+    },
+    {
+        name: "Rainbow Roll",
+        ingredients: ["avocado", "cucumber", "crab meat", "tuna", "salmon", "yellowtail"],
+        price: 16,
+        emoji: "🌈",
+        id: 2
+    },
+    {
+        name: "Spicy Tuna Roll",
+        ingredients: ["tuna", "spicy mayo", "cucumber"],
+        price: 12,
+        emoji: "🍣",
+        id: 3
+    }
+]
